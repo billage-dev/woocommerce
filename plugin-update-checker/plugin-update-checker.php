a21e046c53802db7049decf6df362dd8b66b5838
etorres_pixup <etorres.pixup@gmail.com>
2022-07-12 13:05:10 +0200

    new version

---
+++ plugin-update-checker/plugin-update-checker.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Update Checker Library 4.11
+ * http://w-shadow.com/
+ *
+ * Copyright 2021 Janis Elsts
+ * Released under the MIT license. See license.txt for details.
+ */
+
+require dirname(__FILE__) . '/load-v4p11.php';
+//$token='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';
+$token="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";
